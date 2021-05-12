876bb5131a18d6d835a56b030bc1fa0ed989ccd1
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2021-05-12 11:44:10 +0430

    Update App.java

--- Factory Pattern/Pattern/App.java
+++ Factory Pattern/Pattern/App.java
@@ -1,8 +1,8 @@
 public class App {
 
     public static void main(String[] args) {
-	    Factory factory = new Factory();
-	    Device myMobile = factory.getDevice("mobile", 15L);
+	Factory factory = new Factory();
+	Device myMobile = factory.getDevice("mobile", 15L);
         Device myComputer = factory.getDevice("computer", 156L);
         Device myTV = factory.getDevice("tv", 165L);
     }
