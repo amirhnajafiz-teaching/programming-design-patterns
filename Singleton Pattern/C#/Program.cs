a014ca7098d09a33d77485111255fd293c609c2b
9831065 <najafizadeh21@gmail.com>
2021-08-19 12:06:09 +0430

    Singleton C# added

---
+++ Singleton Pattern/C#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var singleton = Singleton.GetInstance(12);
+            Console.WriteLine(singleton.GetCode());
+
+            var obj = Singleton.GetInstance(24);
+            Console.WriteLine(obj.GetCode());
+        }
+    }
+}
---
+++ Singleton Pattern/C#/Singleton.cs
@@ -0,0 +1,22 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private Singleton(int init) {
+            _code = init;
+        }
+
+        private static Singleton _instance = null;
+        private readonly int _code;
+
+        public int GetCode()
+        {
+            return _code;
+        }
+
+        public static Singleton GetInstance(int init)
+        {
+            return _instance ?? (_instance = new Singleton(init));
+        }
+    }
+}
