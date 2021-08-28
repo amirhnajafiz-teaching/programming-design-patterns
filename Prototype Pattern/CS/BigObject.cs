bf3f553b4ce0f5770682b0775415f56bafb4be59
9831065 <najafizadeh21@gmail.com>
2021-08-28 11:41:20 +0430

    Prototype CS version added

---
+++ Prototype Pattern/CS/BigObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class BigObject
+    {
+        private readonly int _code;
+        private string _data;
+
+        public BigObject(int code, string data)
+        {
+            this._code = code;
+            this._data = data;
+        }
+
+        public BigObject ResetWith(string data)
+        {
+            this._data = data;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return this._code + " " + this._data;
+        }
+
+        public BigObject Copy()
+        {
+            return (BigObject)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype Pattern/CS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prototype
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var obj = new BigObject(20, "First");
+            var newObj = obj.Copy().ResetWith("Second");
+            
+            Console.WriteLine(obj);
+            Console.WriteLine(newObj);
+        }
+    }
+}
