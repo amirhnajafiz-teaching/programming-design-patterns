d368c1e869ccd47d8f2fe6adc49d75174cba0aa2
9831065 <najafizadeh21@gmail.com>
2021-08-29 10:21:09 +0430

    Builder pattern CS version added

---
+++ Builder Pattern/CS/IObject.cs
@@ -0,0 +1,39 @@
+﻿namespace Builder
+{
+    public class IObject
+    {
+        private int _code;
+        private string _meta;
+        private string _id;
+        private bool _nullAble;
+
+        public IObject()
+        {
+            // Empty Construct
+        }
+
+        public IObject SetCode(int code)
+        {
+            this._code = code;
+            return this;
+        }
+
+        public IObject SetMeta(string meta)
+        {
+            this._meta = meta;
+            return this;
+        }
+
+        public IObject SetId(string id)
+        {
+            this._id = id;
+            return this;
+        }
+
+        public IObject SetNullAble(bool nullAble)
+        {
+            this._nullAble = nullAble;
+            return this;
+        }
+    }
+}
---
+++ Builder Pattern/CS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Builder
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var myObj = new IObject().SetCode(12).SetNullAble(false).SetId("25552X").SetMeta("Header:Location");
+            var newObj = new IObject().SetNullAble(true).SetId("TT66").SetMeta("Empty space");
+            
+            Console.WriteLine(myObj);
+            Console.WriteLine(newObj);
+        }
+    }
+}
